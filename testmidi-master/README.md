dc6ec053ae0708a8c3e304f81545178193a77772
105590047 <t105590047@ntut.org.tw>
2018-12-24 22:50:38 +0800

    upload

---
+++ testmidi-master/README.md
@@ -0,0 +1,3 @@
+Testing MIDI with Vexflow and MIDI.js
+
+The demo site is up at http://testmidi.herokuapp.com/
---
+++ testmidi-master/web.js
@@ -0,0 +1,54 @@
+// express JS
+var port = process.env.PORT || 8080;
+
+// include ExpressJS framework
+var express = require('express');
+var fs = require('fs');
+var util = require('./util.js');
+
+// create a server from the express
+var app = express.createServer();
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/static'));
+app.listen(port);
+
+var basepath = 'http://localhost:8080';
+if (port != process.env.PORT) {
+    basepath = 'http://localhost:8080';
+}
+
+app.get('/', function (req, res) {
+    var page = req.param('page', '');
+    if (page == '') {
+        page = 'index.html';
+    }
+
+    var templateValues = {
+        basepath: basepath
+    };
+    if (page == 'test.html') {
+        var song = req.param('song', 'song1');
+        templateValues.song = song;
+    }
+
+    var data = fs.readFileSync(page, 'utf8');
+    data = util.replaceTemplateValue(data, templateValues);
+    res.end(data);
+});
+
+
+
+
+var midisong = require('./midisong.js');
+app.get('/midi', function (req, res) {
+    res.writeHead(200, {
+        "Content-Type": "audio/midi",
+        'Access-Control-Allow-Origin': '*'
+    });
+    var buffer = midisong.getMidiSong(req.param('song'));
+    var result = {
+        data: 'data:audio/midi;base64,' + new Buffer(buffer, 'binary').toString('base64'),
+        maxChannel: 3
+    };
+    res.end(JSON.stringify(result));
+});
